9cbe580c88aa803217e6b6ccc99ed6bd2acf4a24
kimuijin <dmlwls2435@naver.com>
2022-12-16 21:29:40 +0900

    Initial commit

---
+++ src/main/kotlin/com/modu/modutranslate/ModuTranslateApplication.kt
@@ -0,0 +1,11 @@
+package com.modu.modutranslate
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModuTranslateApplication
+
+fun main(args: Array<String>) {
+    runApplication<ModuTranslateApplication>(*args)
+}
---
+++ src/test/kotlin/com/modu/modutranslate/ModuTranslateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.modu.modutranslate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ModuTranslateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
